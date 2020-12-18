c6f01bfb19b83b6b53cdb9e18950be5ff09a20af
Joe Chaiet <jchaiet@hotmail.com>
2020-12-18 14:52:23 -0500

    Deploying

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
-const cors= require('cors');
+const cors = require('cors');
+const path = require('path');
+
+//Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//Anything that doesn't match above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
 
 //Create the server
 const app = express();
